0b96d54fa1e23e235cce386a41bd6d401d85a0f6
viliam.simko <viliam.simko@d2388a58-e3b3-11dd-8fb1-9960f2a117f8>
2009-11-19 13:58:57 +0000

    endpoint design

---
+++ trunk/GamaRepository/endpoint/design/page-header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>GAMA Repository endpoint (db:<?php echo GAMA_Store::getDatabaseName() ?>)</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<meta name="Author" content="Viliam Simko vlx@matfyz.cz"/>
+	<link rel="stylesheet" href="design/endpoint.css" type="text/css"/>	
+</head>
+<body>
+<div id="fixedmenu">
+	<a href="index.php">&#11013;&nbsp;Go back to the endpoint</a>
+</div>
