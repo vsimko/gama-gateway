2898db1226b6864d33772c083300ee92e69a270d
viliam.simko <viliam.simko@d2388a58-e3b3-11dd-8fb1-9960f2a117f8>
2009-11-19 13:58:57 +0000

    endpoint design

---
+++ GamaRepository/endpoint/design/page-footer.php
@@ -0,0 +1,29 @@
+<!-- ======================================================================= -->
+<!-- W3C notice about valid XHTML and CSS -->
+<!-- ======================================================================= -->
+<div style="text-align:left; margin:40px">
+<a href="http://validator.w3.org/check?uri=referer">
+	<img style="border:0;width:88px;height:31px"
+		src="http://www.w3.org/Icons/valid-xhtml10-blue" alt="Valid XHTML 1.0 Strict"/>
+</a>
+<a href="http://jigsaw.w3.org/css-validator/">
+	<img style="border:0;width:88px;height:31px"
+		src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!" />
+</a>
+</div>
+
+<!-- ======================================================================= -->
+<!-- Google analytics -->
+<!-- ======================================================================= -->
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-5565048-4");
+pageTracker._trackPageview();
+</script>
+
+<!-- ======================================================================= -->
+</body>
+</html>
